2095420c7c0d16cdd5899af4091e03da63d9063e
Viiic98 <xathovic@gmail.com>
2020-07-16 17:41:58 -0500

    Modify dot to matmul

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Matrix multiplication with Numpy """
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """ Function that performs matrix multiplication """
-    return mat1.dot(mat2)
+    return np.matmul(mat1, mat2)
