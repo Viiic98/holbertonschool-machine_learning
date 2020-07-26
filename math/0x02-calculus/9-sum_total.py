b85d09ab17886bc37f4853d50fb0251ed6dfdc12
Viiic98 <xathovic@gmail.com>
2020-07-26 17:00:56 -0500

    Fix a condition and pep8

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,6 +6,6 @@ def summation_i_squared(n):
     """ Recursive function """
     if (type(n) != int and type(n) != float):
         return None
-    if (n < 0):
+    if (n < 1):
         return None
-    return sum(map(lambda i : i*i, range(n + 1)))
+    return sum(map(lambda i: i*i, range(n + 1)))
