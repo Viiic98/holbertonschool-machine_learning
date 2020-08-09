727049ccfb0a31ae55617882d3a130d985bcb912
Viiic98 <xathovic@gmail.com>
2020-08-09 18:23:14 -0500

    Add some spaces to the file

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class DeepNeuralNetwork():
-    """ defines a deep neural network performing binary classification """
+    """ defines a deep neural network """
     def __init__(self, nx, layers):
         """ Class constructor
 
@@ -20,14 +20,18 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list or not layers:
             raise TypeError("layers must be a list of positive integers")
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
+
         for l in range(self.L):
             if type(layers[l]) is not int or layers[l] <= 0:
                 raise TypeError("layers must be a list of positive integers")
+
             w = "W" + str(l + 1)
             b = "b" + str(l + 1)
+
             if l == 0:
                 self.weights[w] = np.random.randn(layers[l], nx)\
                                                  * np.sqrt(2. / nx)
