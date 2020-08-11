691a4f10dea5871e3208a196a69233aa02a4bfcd
Viiic98 <xathovic@gmail.com>
2020-08-11 14:52:00 -0500

    Add one hot decoder

---
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" One hot decoder """
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """ converts a one-hot matrix into a vector of labels
+        @one_hot: one-hot encoded numpy.ndarray with shape (classes, m)
+            @classes is the maximum number of classes
+            @m is the number of examples
+    """
+    if type(one_hot) is not np.ndarray:
+        return None
+    if one_hot is None:
+        return None
+    if len(one_hot) == 0 or len(one_hot[0]) == 0:
+        return None
+    Y = []
+    for i in range(len(one_hot[0])):
+        for j in range(len(one_hot)):
+            if one_hot[j][i] == 1:
+                Y.append(j)
+    return np.array(Y)
