4f67345eb7605b240583936d0559a3e8d0020893
Viiic98 <xathovic@gmail.com>
2020-08-18 18:58:25 -0500

    Modify return

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -19,5 +19,7 @@ def evaluate(X, Y, save_path):
         y_pred = tf.get_collection('y_pred')[0]
         loss = tf.get_collection('loss')[0]
         accuracy = tf.get_collection('accuracy')[0]
-        return sess.run([y_pred, accuracy, loss],
-                        feed_dict={x: X, y: Y})
+        pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accu_val = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_val = sess.run(loss, feed_dict={x: X, y: Y})
+        return pred, accu_val, loss_val
