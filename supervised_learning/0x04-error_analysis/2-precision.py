cdba5184db5ecb0d9e7903bf9c0ec245b5f48ec0
Viiic98 <xathovic@gmail.com>
2020-08-26 22:43:44 -0500

    Add presicion

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Presicion """
+import numpy as np
+
+
+def precision(confusion):
+    """ calculates the precision for each class in a
+        confusion matrix
+
+        @confusion: is a confusion numpy.ndarray of shape
+                    (classes, classes) where row indices represent
+                    the correct labels and column indices represent
+                    the predicted labels
+        @classes: is the number of classes
+        Returns: a numpy.ndarray of shape (classes,) containing the
+                 precision of each class
+    """
+    prec = np.zeros(confusion.shape[0])
+    for i in range(confusion.shape[0]):
+        tp = confusion[i][i]
+        fp = np.sum(confusion, axis=0) - tp
+        prec[i] = tp / (tp + fn[i])
+    return prec
