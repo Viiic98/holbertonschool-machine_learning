dbe5a5501e3b9c8016a1b28c2de6cb9ca6719a3a
Viiic98 <xathovic@gmail.com>
2020-08-28 13:31:43 -0500

    Fix keys of A

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -22,22 +22,22 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     """
     drop_out = {}
     A = X
-    drop_out['A0'] = A / keep_prob
     for i in range(L):
         if i > 0:
             d = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob)\
                 .astype(int)
             drop_out['D' + str(i)] = d
             A = np.multiply(A, d)
-        A /= keep_prob
+            A /= keep_prob
+        drop_out['A' + str(i)] = A
         Z = np.dot(weights['W' + str(i + 1)], A) + weights['b' + str(i + 1)]
         # Activation
         # Softmax
         if i + 1 == L:
             exp = np.exp(Z)
             A = exp / np.sum(exp, axis=0, keepdims=True)
+            drop_out['A' + str(i + 1)] = A
         else:
             # Tanh Activation
             A = np.tanh(Z)
-        drop_out['A' + str(i + 1)] = A
     return drop_out
