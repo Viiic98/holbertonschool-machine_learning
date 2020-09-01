ec21213a6e795411329e319f2ccc36156c61315b
Viiic98 <xathovic@gmail.com>
2020-09-01 13:16:41 -0500

    Fix output in model creation

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -17,25 +17,21 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         You are not allowed to use the Sequential class
         Returns: the keras model
     """
-    inputs = inp = K.Input(shape=(nx,))
-    outputs = []
+    inputs = K.Input(shape=(nx,))
+    inp = inputs
     for i in range(len(layers)):
         if i + 1 < len(layers):
             layer = K.layers.Dense(layers[i],
                                    activation=activations[i],
-                                   input_shape=(nx,),
                                    kernel_regularizer=K.regularizers.l2(
                                                         lambtha))(inp)
             dropout = (K.layers.Dropout(1 - keep_prob))(layer)
-            outputs.append(layer)
-            outputs.append(dropout)
             inp = dropout
         else:
             layer = K.layers.Dense(layers[i],
                                    activation=activations[i],
-                                   input_shape=(nx,),
                                    kernel_regularizer=K.regularizers.l2(
                                                         lambtha))(inp)
-            outputs.append(layer)
-    model = K.Model(inputs=inputs, outputs=outputs)
+            output = layer
+    model = K.Model(inputs=inputs, outputs=output)
     return model
