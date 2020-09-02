e2fe8f141add7a5c395cd4244fe99bec83e286f3
Viiic98 <xathovic@gmail.com>
2020-09-01 19:02:48 -0500

    Add prediction to model

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Makes a prediction """
+
+
+def predict(network, data, verbose=False):
+    """ makes a prediction using a neural network
+
+        @network: is the network model to make the
+                  prediction with
+        @data: is the input data to make the prediction with
+        @verbose: is a boolean that determines if output should
+                  be printed during the prediction process
+        Returns: the prediction for the data
+    """
+    pred = network.predict(data, verbose=verbose)
+    return pred
