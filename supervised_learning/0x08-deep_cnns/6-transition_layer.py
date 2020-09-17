2d50656d103fb63220dfc83ecfa8c5e5476c5b6f
Viiic98 <xathovic@gmail.com>
2020-09-17 03:22:59 -0500

    Remove a change and remove activation from convolution

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -17,13 +17,12 @@ def transition_layer(X, nb_filters, compression):
     filters within the output, respectively
     """
     init = K.initializers.he_normal()
-    batch = K.layers.BatchNormalization(axis=3)(X)
+    batch = K.layers.BatchNormalization()(X)
     act = K.layers.Activation(K.activations.relu)(batch)
     conv = K.layers.Conv2D(int(nb_filters * compression), (1, 1),
                            padding='same',
                            strides=1,
-                           kernel_initializer=init,
-                           activation='relu')(act)
+                           kernel_initializer=init)(act)
     avg_pool = K.layers.AveragePooling2D(pool_size=(2, 2),
                                          strides=2)(conv)
     return avg_pool, int(nb_filters * compression)
