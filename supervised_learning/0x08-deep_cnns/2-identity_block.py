b58bd1e0f0109bfb5801491e0488a74af9c54287
Viiic98 <xathovic@gmail.com>
2020-09-17 03:31:16 -0500

    Add weight initialization

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -18,13 +18,17 @@ def identity_block(A_prev, filters):
         Returns: the activated output of the identity block
     """
     F11, F3, F12 = filters
-    layer_0 = K.layers.Conv2D(F11, (1, 1), padding='same')(A_prev)
+    init = K.initializers.he_normal()
+    layer_0 = K.layers.Conv2D(F11, (1, 1), padding='same',
+                              kernel_initializer=init)(A_prev)
     batch = K.layers.BatchNormalization()(layer_0)
     act = K.layers.Activation(K.activations.relu)(batch)
-    layer_1 = K.layers.Conv2D(F3, (3, 3), padding='same')(act)
+    layer_1 = K.layers.Conv2D(F3, (3, 3), padding='same',
+                              kernel_initializer=init)(act)
     batch = K.layers.BatchNormalization()(layer_1)
     act = K.layers.Activation(K.activations.relu)(batch)
-    layer_2 = K.layers.Conv2D(F12, (1, 1), padding='same')(act)
+    layer_2 = K.layers.Conv2D(F12, (1, 1), padding='same',
+                              kernel_initializer=init)(act)
     batch = K.layers.BatchNormalization()(layer_2)
     add = K.layers.Add()([batch, A_prev])
     act = K.layers.Activation(K.activations.relu)(add)
