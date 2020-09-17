b91fd72141ee3342dc3315b6314293efb3841cf6
Viiic98 <xathovic@gmail.com>
2020-09-17 03:33:15 -0500

    Remove activation and add weight initialization

--- supervised_learning/0x08-deep_cnns/3-projection_block.py
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -21,21 +21,22 @@ def projection_block(A_prev, filters, s=2):
     Returns: the activated output of the projection block
     """
     F11, F3, F12 = filters
+    init = K.initializers.he_normal()
     layer_0 = K.layers.Conv2D(F11, (1, 1), padding='same',
                               strides=s,
-                              activation='relu')(A_prev)
+                              kernel_initializer=init)(A_prev)
     batch = K.layers.BatchNormalization()(layer_0)
     act = K.layers.Activation(K.activations.relu)(batch)
     layer_1 = K.layers.Conv2D(F3, (3, 3), padding='same',
-                              activation='relu')(act)
+                              kernel_initializer=init)(act)
     batch = K.layers.BatchNormalization()(layer_1)
     act = K.layers.Activation(K.activations.relu)(batch)
     layer_2 = K.layers.Conv2D(F12, (1, 1), padding='same',
-                              activation='relu')(act)
+                              kernel_initializer=init)(act)
     batch = K.layers.BatchNormalization()(layer_2)
     layer_3 = K.layers.Conv2D(F12, (1, 1), padding='same',
                               strides=s,
-                              activation='relu')(A_prev)
+                              kernel_initializer=init)(A_prev)
     batch_short = K.layers.BatchNormalization()(layer_3)
     add = K.layers.Add()([batch, batch_short])
     act = K.layers.Activation(K.activations.relu)(add)
