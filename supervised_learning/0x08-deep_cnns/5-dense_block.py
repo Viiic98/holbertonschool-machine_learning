7e7976642f5e6880d5ac1290aaa9278f1794cd8f
Viiic98 <xathovic@gmail.com>
2020-09-17 03:24:38 -0500

    Remove activation from convolutionan layers

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -26,14 +26,12 @@ def dense_block(X, nb_filters, growth_rate, layers):
         act = K.layers.Activation(K.activations.relu)(batch)
         conv = K.layers.Conv2D(growth_rate * 4, (1, 1), padding='same',
                                strides=1,
-                               kernel_initializer=init,
-                               activation='relu')(act)
+                               kernel_initializer=init)(act)
         batch = K.layers.BatchNormalization()(conv)
         act = K.layers.Activation(K.activations.relu)(batch)
         conv = K.layers.Conv2D(growth_rate, (3, 3), padding='same',
                                strides=1,
-                               kernel_initializer=init,
-                               activation='relu')(act)
+                               kernel_initializer=init)(act)
         X = K.layers.concatenate([X, conv], axis=3)
         nb_filters += growth_rate
     return X, nb_filters
