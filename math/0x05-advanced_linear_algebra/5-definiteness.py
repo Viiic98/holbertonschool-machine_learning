fa6aea543e4d4e769b3b8377a3140d6e402c3f9d
Viiic98 <xathovic@gmail.com>
2020-10-22 14:53:56 -0500

    Add definiteness

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+""" Definiteness """
+import numpy as np
+
+
+def definiteness(matrix):
+    """ calculates the definiteness of a matrix
+
+        - matrix is a numpy.ndarray of shape (n, n) whose definiteness
+          should be calculated
+        - If matrix is not a numpy.ndarray, raise a TypeError with
+          the message matrix must be a numpy.ndarray
+        - If matrix is not a valid matrix, return None
+        Return: the string Positive definite, Positive semi-definite,
+                Negative semi-definite, Negative definite, or Indefinite if
+                the matrix is positive definite, positive semi-definite,
+                negative semi-definite, negative definite of indefinite,
+                respectively
+        - If matrix does not fit any of the above categories, return None
+    """
+    if type(matrix) != np.ndarray:
+        raise TypeError("matrix must be a numpy.ndarray")
+    if matrix.shape[0] == 0:
+        return None
+    if matrix.shape[0] != matrix.shape[1]:
+        return None
+    w, v = np.linalg.eig(matrix)
+    # count to classify
+    pos = 0
+    sem_pos = 0
+    neg = 0
+    sem_neg = 0
+    for x in w:
+        if x > 0:
+            pos += 1
+        if x >= 0:
+            sem_pos += 1
+        if x < 0:
+            neg += 1
+        if x <= 0:
+            sem_neg += 1
+    # All eigenvalues are > 0
+    if pos == len(w):
+        return "Positive definite"
+    # All eigenvalues are >= 0
+    if sem_pos == len(w):
+        return "Positive semi-definite"
+    # All eigenvalues are < 0
+    if neg == len(w):
+        return "Negative definite"
+    # All eigenvalues are <= 0
+    if sem_neg == len(w):
+        return "Negative semi-definite"
+    return "Indefinite"
