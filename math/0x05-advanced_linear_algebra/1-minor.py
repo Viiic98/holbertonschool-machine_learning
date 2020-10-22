e70bbc0d985968acec6e59c13eebe2b18dcb7b05
Viiic98 <xathovic@gmail.com>
2020-10-22 13:15:09 -0500

    Fix minor

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -2,6 +2,37 @@
 """ Minor Matrix """
 
 
+def determinant(matrix):
+    """ calculates the determinant of a matrix
+
+        - matrix is a list of lists whose determinant
+          should be calculated
+        - If matrix is not a list of lists, raise a TypeError
+          with the message matrix must be a list of lists
+        - If matrix is not square, raise a ValueError with the
+          message matrix must be a square matrix
+        - The list [[]] represents a 0x0 matrix
+        Returns: the determinant of matrix
+    """
+    if len(matrix[0]) == 0:
+        return 1
+    if len(matrix[0]) == 1:
+        return matrix[0][0]
+    det = 0
+    if len(matrix) == 2:
+        det = matrix[0][0] * matrix[1][1]
+        det -= matrix[0][1] * matrix[1][0]
+        return det
+    sign = 1
+    for j in range(len(matrix[0])):
+        tmp = matrix[1:]
+        for i in range(len(tmp)):
+            tmp[i] = tmp[i][0:j] + tmp[i][j + 1:]
+        det += (sign * matrix[0][j]) * determinant(tmp)
+        sign *= -1
+    return det
+
+
 def minor(matrix):
     """ calculates the minor matrix of a matrix
 
@@ -35,12 +66,13 @@ def minor(matrix):
                 tmp[k] = tmp[k][0:j] + tmp[k][j + 1:]
             if len(tmp) > 1:
                 if len(tmp) > 2:
-                    minor(tm)
-                a = tmp[0][0]
-                b = tmp[0][1]
-                c = tmp[1][0]
-                d = tmp[1][1]
-                minor_matrix[i][j] = a * d - b * c
+                    minor_matrix[i][j] = determinant(tmp)
+                else:
+                    a = tmp[0][0]
+                    b = tmp[0][1]
+                    c = tmp[1][0]
+                    d = tmp[1][1]
+                    minor_matrix[i][j] = a * d - b * c
             else:
                 minor_matrix[i][j] = tmp[0][0]
     return minor_matrix
