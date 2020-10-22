c7fb678db769a50014379b7a62741d468f58e26d
Viiic98 <xathovic@gmail.com>
2020-10-22 13:22:25 -0500

    Add validations

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -44,6 +44,10 @@ def minor(matrix):
           with the message matrix must be a non-empty square matrix
         Returns: the minor matrix of matrix
     """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a list of lists")
+    if type(matrix[0]) is not list:
+        raise TypeError("matrix must be a list of lists")
     if len(matrix) == 0:
         raise ValueError("matrix must be a non-empty square matrix")
     if len(matrix) != len(matrix[0]):
