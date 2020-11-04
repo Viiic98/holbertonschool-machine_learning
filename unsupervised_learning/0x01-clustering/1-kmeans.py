4c79fce9f0b452db161c1ccbc6a303625a2ea71d
Viiic98 <xathovic@gmail.com>
2020-11-04 10:27:26 -0500

    Add validations

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -31,6 +31,12 @@ def kmeans(X, k, iterations=1000):
             - clss is a numpy.ndarray of shape (n,) containing the index of the
               cluster in C that each data point belongs to
     """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(k) is not int or k < 1:
+        return None
+    if type(iterations) is not int or iterations < 1:
+        return None
     n, d = X.shape
     min = np.amin(X, axis=0)
     max = np.amax(X, axis=0)
