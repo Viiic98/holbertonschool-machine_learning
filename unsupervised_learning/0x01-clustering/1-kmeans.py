4f0581231ab118f37d85cb6d44f2c71bb24d0feb
Viiic98 <xathovic@gmail.com>
2020-11-04 09:56:50 -0500

    Add task 1

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -1,1 +1,58 @@
 #!/usr/bin/env python3
+""" K-means """
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def closest_centroid(points, centroids):
+    """returns an array containing the index to the nearest centroid for each point"""
+    distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
+    return np.argmin(distances, axis=0)
+
+def move_centroids(points, closest, centroids):
+    """returns the new centroids assigned from the points closest to them"""
+    return np.array([points[closest==k].mean(axis=0) for k in range(centroids.shape[0])])
+
+def kmeans(X, k, iterations=1000):
+    """ performs K-means on a dataset
+
+        - X is a numpy.ndarray of shape (n, d) containing the dataset
+            - n is the number of data points
+            - d is the number of dimensions for each data point
+        - k is a positive integer containing the number of clusters
+        - iterations is a positive integer containing the maximum number of
+          iterations that should be performed
+        - If no change in the cluster centroids occurs between iterations,
+          your function should return
+        - Initialize the cluster centroids using a multivariate uniform
+          distribution (based on0-initialize.py)
+        - If a cluster contains no data points during the update step,
+          reinitialize its centroid
+        Returns: C, clss, or None, None on failure
+            - C is a numpy.ndarray of shape (k, d) containing the centroid
+              means for each cluster
+            - clss is a numpy.ndarray of shape (n,) containing the index of the
+              cluster in C that each data point belongs to
+    """
+    n, d = X.shape
+    min = np.amin(X, axis=0)
+    max = np.amax(X, axis=0)
+    centroids = np.random.uniform(low=min, high=max, size=(k, d))
+    f = 0
+    c = move_centroids(X, closest_centroid(X, centroids), centroids)
+    for i in range(iterations):
+        if f == 0 and np.isnan(c.sum()):
+            centroids = np.random.uniform(low=min, high=max, size=(k, d))
+            c = move_centroids(X, closest_centroid(X, centroids), centroids)
+            i -= 1
+        else:
+            f = 1
+            closes = closest_centroid(X, centroids)
+            copy = np.copy(centroids)
+            for c in range(k):
+                idx = np.where(closes == c)
+                mean = X[idx].mean(axis=0)
+                centroids[c] = mean
+            if np.array_equal(copy, centroids):
+                break
+    return centroids, closes
