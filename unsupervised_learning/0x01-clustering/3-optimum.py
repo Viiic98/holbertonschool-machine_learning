9fafdb8f6034441cbdf89bc06f3466dae2dcf70b
Viiic98 <xathovic@gmail.com>
2020-11-04 15:29:26 -0500

    Fix validations

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -23,15 +23,16 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
+    if kmax is None:
+        kmax = X.shape[0]
     if type(kmin) is not int or type(kmax) is not int:
         return None, None
     if kmin < 1 or kmax < 1:
         return None, None
-    if kmin > kmax:
+    if kmin >= kmax:
         return None, None
     if type(iterations) is not int or iterations < 1:
         return None, None
-
     results = []
     d_vars = []
     for k in range(kmin, kmax + 1):
