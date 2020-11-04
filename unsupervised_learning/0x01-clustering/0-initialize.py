96cb8dcc728c2aa631c8cdabcb2b20671c2c89f3
Viiic98 <xathovic@gmail.com>
2020-11-04 10:22:55 -0500

    Add validations

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -20,6 +20,11 @@ def initialize(X, k):
         Returns: a numpy.ndarray of shape (k, d) containing the initialized
                  centroids for each cluster, or None on failure
     """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(k) is not int or k < 1:
+        return None
+    print(X.shape)
     n, d = X.shape
     min = np.amin(X, axis=0)
     max = np.amax(X, axis=0)
