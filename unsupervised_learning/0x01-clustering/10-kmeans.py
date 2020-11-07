a854dedaead15b894363d4ebdd2bd3e815e85e15
Viiic98 <xathovic@gmail.com>
2020-11-06 19:58:50 -0500

    Add kmeans with sklearn

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+""" K-means SKLEARN """
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """ performs K-means on a dataset
+
+        X is a numpy.ndarray of shape (n, d) containing the dataset
+        k is the number of clusters
+        Returns: C, clss
+            - C is a numpy.ndarray of shape (k, d) containing the
+              centroid means for each cluster
+            - clss is a numpy.ndarray of shape (n,) containing the
+              index of the cluster in C that each data point belongs to
+    """
+    C, clss, _ = sklearn.cluster.k_means(X, k)
+    return C, clss
