bd20a3e94122b57f0d0320677e12d43b06e38cde
Viiic98 <xathovic@gmail.com>
2020-11-12 22:13:22 -0500

    Add validation

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -15,25 +15,35 @@ def absorbing(P):
     """
     if type(P) is not np.ndarray or len(P.shape) != 2 or len(P) != len(P[0]):
         return False
+    if P.sum(axis=1).all() != 1:
+        return False
+    # Diagonal
     diag = np.diagonal(P)
+    # Absorbing states
     abs_states = np.where(diag == 1)[0]
+    # No absorbing states
     nabs_sta = np.where(diag != 1)[0]
     if len(abs_states) == 0:
         return False
     if len(nabs_sta) == 0:
         return True
     while True:
+        # Select states that could be transformed in an absorbing state
         tr = np.where(np.logical_and(P[:, abs_states] > 0,
                                      P[:, abs_states] < 1))[0]
         tr = np.unique(tr)
+        # If any value could be transformed just return False
         if len(tr) == 0:
             return False
         c = np.copy(nabs_sta)
+        # Check for the last no abs if it could be transformed
         if len(nabs_sta) == 1 and nabs_sta in tr:
             return True
+        # Remove states that could be transformed to absorbing state
         nabs_sta = np.delete(nabs_sta, np.where(nabs_sta == tr)[0])
         if np.array_equal(nabs_sta, c):
             return False
         if len(nabs_sta) == 0:
             return True
+        # Add the non-absorbing that could be transformed
         abs_states = np.unique(np.append(abs_states, tr))
