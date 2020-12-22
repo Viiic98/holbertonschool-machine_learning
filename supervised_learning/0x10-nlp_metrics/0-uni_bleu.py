8ddd5808917882c156cb56d3d6d2238cafc59665
Viiic98 <xathovic@gmail.com>
2020-12-22 15:26:45 -0500

    Add unigram bleu

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,39 @@
+#!/u1sr/bin/env python3
+""" Unigram BLEU score """
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """ calculates the unigram BLEU score for a sentence
+
+        - references is a list of reference translations
+            - each reference translation is a list of the
+              words in the translation
+         -sentence is a list containing the model proposed sentence
+        Returns: the unigram BLEU score
+    """
+    c = len(sentence)
+    # Calculate r, closest length reference to candidate
+    r_list = np.array([len(ref) - c for ref in references])
+    # Take indices whit the same distance
+    mask = np.where(r_list == r_list.min())
+    # Apply mask and take the minimum length in masked references
+    r = np.array([len(ref) for ref in references])[mask].min()
+
+    candidate = {x: sentence.count(x) for x in sentence}
+    max_match = 0
+    for ref in references:
+        match = 0
+        ref_dict = {x: ref.count(x) for x in ref}
+        for key in ref_dict.keys():
+            if key in candidate:
+                match += 1
+        if match > max_match:
+            max_match = match
+    P = max_match / c
+
+    if c > r:
+        BP = 1
+    else:
+        BP = np.exp(1-(r / c))
+    return BP * P
---
+++ supervised_learning/0x10-nlp_metrics/README.md
@@ -0,0 +1,12 @@
+# Natural Language Processing - Evaluation Metrics
+
+### Tasks
+
+#### [Unigram BLEU score](./0-uni_bleu.py)
+- Write the function def uni_bleu(references, sentence): that calculates the unigram BLEU score for a sentence
+
+#### [N-gram BLEU score](./1-ngram_bleu.py)
+- Write the function def ngram_bleu(references, sentence, n): that calculates the n-gram BLEU score for a sentence
+
+#### [Cumulative N-gram BLEU score](./2-cumulative_bleu.py)
+- Write the function def cumulative_bleu(references, sentence, n): that calculates the cumulative n-gram BLEU score for a sentence
