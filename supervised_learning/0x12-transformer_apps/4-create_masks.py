ebc975674ac1a06b015eda842710472425b67bc2
Viiic98 <xathovic@gmail.com>
2021-01-14 10:07:09 -0500

    Fix masks

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -5,11 +5,20 @@ import tensorflow as tf
 
 def create_masks(inputs, target):
     """ creates all masks for training/validation """
-    encoder_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
-    encoder_mask = encoder_mask[:, tf.newaxis, tf.newaxis, :]
-    decoder_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
-    decoder_mask = decoder_mask[:, tf.newaxis, tf.newaxis, :]
-    size = target.shape[1]
-    look_ahead_mask = 1 - tf.linalg.band_part(tf.ones(
-        (target.shape[0], 1, size, size)), -1, 0)
-    return encoder_mask, look_ahead_mask, decoder_mask
+    inputs = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    target = tf.cast(tf.math.equal(target, 0), tf.float32)
+    # Encoder padding mask
+    enc_padding_mask = inputs[:, tf.newaxis, tf.newaxis, :]
+
+    # Used in the 2nd attention block in the decoder.
+    # This padding mask is used to mask the encoder outputs.
+    dec_padding_mask = inputs[:, tf.newaxis, tf.newaxis, :]
+
+    # Used in the 1st attention block in the decoder.
+    # It is used to pad and mask future tokens in the input received by
+    # the decoder.
+    dec_target_mask = target[:, tf.newaxis, tf.newaxis, :]
+    x, y = target.shape
+    look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((x, 1, y, y)), -1, 0)
+    combined_mask = tf.maximum(dec_target_mask, look_ahead_mask)
+    return enc_padding_mask, combined_mask, dec_padding_mask
